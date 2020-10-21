71f79141da059be986696f529f781c21f3c5aadb
Hayman Abdeljabbar <habdelj@uwo.ca>
2020-10-20 22:34:41 -0400

    Added Enemys
    Still need to get to animate

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Enemycontroller enemycontroller;
+    private Animator animator;
+    private NavMeshAgent nav;
+    private void Awake() 
+    {
+        nav = GetComponent<NavMeshAgent>();
+        enemycontroller = GetComponent<Enemycontroller>();
+        enemycontroller.OnAttack += Enemycontroller_OnAttack;
+        animator.GetComponentInChildren<Animator>();
+    }
+
+    private void Update() {
+        var x = nav.velocity.x;
+        var y = nav.velocity.y;
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY",y);
+    }
+
+    private void Enemycontroller_OnAttack(Transform target)
+    {
+        nav.SetDestination(target.transform.position);
+    }
+}
---
+++ Assets/Scripts/Enemy/Enemycontroller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemycontroller : MonoBehaviour
+{
+    private Animator animator;
+    public event Action<Transform> OnAttack = delegate {};
+    private void OnTriggerEnter(Collider other) {
+        var player = other.GetComponent<PlayerMovment>();
+        
+        if (player != null)
+        {
+            OnAttack(player.transform);
+            GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
+            
+        }
+    }
+}
